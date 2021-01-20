179c06f14161ccdb32d5924e03e8e36e756c2885
abhayull2008 <Abhayullas2008@gmail.com>
2021-01-20 16:03:12 +0530

    added log6

--- sketch.js
+++ sketch.js
@@ -23,6 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    log6 = new Log(50,100,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -58,6 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
